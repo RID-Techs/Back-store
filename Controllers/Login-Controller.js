5ec261a310c833b9e0d73924829d458c472dfa50
RID-Techs <bossbossa954@gmail.com>
2024-05-27 16:39:57 +0000

    Hallo

--- Controllers/Login-Controller.js
+++ Controllers/Login-Controller.js
@@ -1,5 +1,5 @@
 const User = require('../Models/Login-Model')
-const {google} = require("googleapis")
+// const {google} = require("googleapis")
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -34,7 +34,7 @@ const Login = async (req, res) => {
     
         res.cookie("tokeno", token, {
             httpOnly: true,
-            sameSite: "lax",
+            sameSite: "none",
             secure: true,
             maxAge: 120000
         })
@@ -53,101 +53,101 @@ const Login = async (req, res) => {
     }
 }
 
-const REDIRECT_URI = 'https://back-store-mkge.onrender.com/auth/google/callback';
+// const REDIRECT_URI = 'https://back-store-mkge.onrender.com/auth/google/callback';
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    REDIRECT_URI
-);
+// const oauth2Client = new google.auth.OAuth2(
+//     process.env.CLIENT_ID,
+//     process.env.CLIENT_SECRET,
+//     REDIRECT_URI
+// );
 
-const initiateGoogleLogin = async (req, res) => {
-    try {
-        const url = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
-        });
-        res.json({ url });
-    } catch (error) {
-        console.error('Error generating Google OAuth URL:', error);
-        res.status(500).json({ errorInit: error });
-    }
-}
+// const initiateGoogleLogin = async (req, res) => {
+//     try {
+//         const url = oauth2Client.generateAuthUrl({
+//         access_type: 'offline',
+//         scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
+//         });
+//         res.json({ url });
+//     } catch (error) {
+//         console.error('Error generating Google OAuth URL:', error);
+//         res.status(500).json({ errorInit: error });
+//     }
+// }
 
-const Google_cb = async (req, res) => {
-    try {
-    const code = req.query.code;
-    const { tokens } = await oauth2Client.getToken(code);
-    oauth2Client.setCredentials(tokens);
+// const Google_cb = async (req, res) => {
+//     try {
+//     const code = req.query.code;
+//     const { tokens } = await oauth2Client.getToken(code);
+//     oauth2Client.setCredentials(tokens);
 
-    const oauth2 = google.oauth2({ version: 'v2', auth: oauth2Client });
-    const { data } = await oauth2.userinfo.get();
+//     const oauth2 = google.oauth2({ version: 'v2', auth: oauth2Client });
+//     const { data } = await oauth2.userinfo.get();
 
-    console.log("From Data :", data)
+//     console.log("From Data :", data)
 
-    // Generate JWT
-    const token = jwt.sign(data, process.env.ACCESS_TOKEN, { expiresIn: '2m' });
-    const RefreshToken = jwt.sign(data, process.env.REFRESH_TOKEN, {expiresIn: "1h"})
+//     // Generate JWT
+//     const token = jwt.sign(data, process.env.ACCESS_TOKEN, { expiresIn: '2m' });
+//     const RefreshToken = jwt.sign(data, process.env.REFRESH_TOKEN, {expiresIn: "1h"})
 
-    res.cookie("tokeno", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "None",
-        maxAge: 120000
-    })
+//     res.cookie("tokeno", token, {
+//         httpOnly: true,
+//         secure: true,
+//         sameSite: "None",
+//         maxAge: 120000
+//     })
 
-    res.cookie("RefreshTokeno", RefreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "None",
-        maxAge: 3600000
-    })
+//     res.cookie("RefreshTokeno", RefreshToken, {
+//         httpOnly: true,
+//         secure: true,
+//         sameSite: "None",
+//         maxAge: 3600000
+//     })
 
-    res.status(200).redirect("https://computers-store.netlify.app/store")
-    } catch (error) {
-        console.error('Error handling Google callback:', error);
-        res.status(500).json({ errorCallb: error });
-    }
-}
+//     res.status(200).redirect("https://computers-store.netlify.app/store")
+//     } catch (error) {
+//         console.error('Error handling Google callback:', error);
+//         res.status(500).json({ errorCallb: error });
+//     }
+// }
 
-const Welcome = async (req, res) => {
-    try {
-        res.status(200).json({Mes: "Welcome dear User !"})
-    } catch (error) {
-        res.status(500).json({Message: "Sorry, Something went wrong !!"})
-    }
-}
+// const Welcome = async (req, res) => {
+//     try {
+//         res.status(200).json({Mes: "Welcome dear User !"})
+//     } catch (error) {
+//         res.status(500).json({Message: "Sorry, Something went wrong !!"})
+//     }
+// }
 
-const RefreshEndPoint = async(req, res) => {
-    try {
-        const { RefreshTokeno } = req.cookies
+// const RefreshEndPoint = async(req, res) => {
+//     try {
+//         const { RefreshTokeno } = req.cookies
 
-    if(!RefreshTokeno) {
-        return res.status(403).json({Refresh_Mes: "No Refresh Token found !"})
-    } else {
-        jwt.verify(RefreshTokeno, process.env.REFRESH_TOKEN, (err, user) => {
-            if(err) {
-                console.log("Token Verification", err)
-            } else {
+//     if(!RefreshTokeno) {
+//         return res.status(403).json({Refresh_Mes: "No Refresh Token found !"})
+//     } else {
+//         jwt.verify(RefreshTokeno, process.env.REFRESH_TOKEN, (err, user) => {
+//             if(err) {
+//                 console.log("Token Verification", err)
+//             } else {
 
-                const NewToken = jwt.sign({data: user}, process.env.ACCESS_TOKEN, {expiresIn: "2m"})
+//                 const NewToken = jwt.sign({data: user}, process.env.ACCESS_TOKEN, {expiresIn: "2m"})
 
-                res.cookie("tokeno", NewToken, {
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: "None",
-                    maxAge: 120000
-                })
+//                 res.cookie("tokeno", NewToken, {
+//                     httpOnly: true,
+//                     secure: true,
+//                     sameSite: "None",
+//                     maxAge: 120000
+//                 })
 
-                res.status(200).json({ message: "Access token refreshed" });
+//                 res.status(200).json({ message: "Access token refreshed" });
                 
-            }
-        })
-    }
-    } catch (error) {
-        res.status(500).json({Mes: error})
-    }
-}
+//             }
+//         })
+//     }
+//     } catch (error) {
+//         res.status(500).json({Mes: error})
+//     }
+// }
 
 
 // const LogOut = async (req, res) => {
@@ -169,4 +169,4 @@ const RefreshEndPoint = async(req, res) => {
 // }
 
 
-module.exports = {Signup, Login, initiateGoogleLogin, Google_cb, Welcome, RefreshEndPoint}
+module.exports = {Signup, Login}
--- Routes/Login-Routes.js
+++ Routes/Login-Routes.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const Auth = require('../Middlewares/Auth')
-const { Signup, Login, initiateGoogleLogin, Google_cb, Welcome, RefreshEndPoint } = require('../Controllers/Login-Controller')
+const { Signup, Login } = require('../Controllers/Login-Controller')
 const checkAuth = require('../Middlewares/cookieAuth')
 const checkTokenAuth = require('../Middlewares/checkToken')
 const router = express.Router()
 
 router.post("/signup", Signup) 
 router.post("/login", Login)
-router.get("/google", initiateGoogleLogin)
-router.get("/google/callback", Google_cb)
-router.get("/welcome", checkAuth, Welcome)
-router.post("/refreshtoken", RefreshEndPoint)
+// router.get("/google", initiateGoogleLogin)
+// router.get("/google/callback", Google_cb)
+// router.get("/welcome", checkAuth, Welcome)
+// router.post("/refreshtoken", RefreshEndPoint)
 // router.post("/logout", checkAuth, LogOut)
 
 module.exports = router
